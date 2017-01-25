524dd66556ab4a01c26b4d07dfabbaafe56ce7ec
Drasek <accarrasco@gmail.com>
2017-01-25 17:28:27 +0000

    almost complete

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var score = 0;
+
+  $('.score').html('Score: ' + score)
+
+  $('.wrong').click(function() {
+    $(this).css('background', '#ff0049').css('cursor', 'default');
+    $(this).siblings().unbind().css('cursor', 'default');
+  });
+
+  $('.right').click(function() {
+    $(this).css('background', '#0ace00').css('cursor', 'default');
+    $(this).siblings().unbind().css('cursor', 'default');
+    score++
+    $('.score').html('Score: ' + score)
+  });
+
+  $(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+})
